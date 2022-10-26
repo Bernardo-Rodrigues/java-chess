284490a3c346f686bc877cef4d071c9e354445dc
Bernardo Rodrigues <bernardo.rodrigues@capco.com>
2022-10-26 16:12:31 -0300

    feat: add Position class

---
+++ src/test/java/boardgame/PositionTest.java
@@ -0,0 +1,12 @@
+package boardgame;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class PositionTest {
+    @Test
+    public void givenAPositionWhenCallToStringThenReturnRowAndColumn(){
+        Position position = new Position(1,2);
+        Assert.assertEquals(position.toString(), "1, 2");
+    }
+}
