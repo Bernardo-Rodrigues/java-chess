36dc1aa5047601f42f620f8c6a50f49b6be97a46
Bernardo Rodrigues <bernardo.rodrigues@capco.com>
2022-10-28 15:36:26 -0300

    feat: add piece movement

--- src/test/java/boardgame/BoardTest.java
+++ src/test/java/boardgame/BoardTest.java
@@ -16,4 +16,24 @@ public class BoardTest {
 
         Assert.assertEquals(board.piece(position), rook);
     }
+
+    @Test
+    public void givenARemovedPieceWhenVerifyThatThereIsAPiecePositionThenReturnFalse(){
+        Board board = new Board(8,8);
+        Rook rook = new Rook(board, Color.BLACK);
+        Position position = new Position(0,0);
+
+        board.placePiece(rook, position);
+        board.removePiece(position);
+
+        Assert.assertEquals(board.thereIsAPiece(position), false);
+    }
+
+    @Test
+    public void givenANonexistingPositionWhenVerifyIfItExistsThenReturnFalse(){
+        Board board = new Board(8,8);
+        Position position = new Position(9,9);
+
+        Assert.assertEquals(board.positionExists(position), false);
+    }
 }
--- src/test/java/chess/ChessMatchTest.java
+++ src/test/java/chess/ChessMatchTest.java
@@ -1,5 +1,6 @@
 package chess;
 
+import boardgame.Position;
 import org.junit.Assert;
 import org.junit.Test;
 import pieces.King;
@@ -11,5 +12,27 @@ public class ChessMatchTest {
 
         Assert.assertTrue(chessMatch.getPieces()[0][4] instanceof King);
         Assert.assertTrue(chessMatch.getPieces()[7][4] instanceof King);
+    }
+
+    @Test
+    public void givenAChessPieceWhenPerformAMoveThenPlaceThePieceCorretly(){
+        ChessMatch chessMatch = new ChessMatch();
+        ChessPosition sourcePosition = new ChessPosition('e', 8);
+        ChessPosition targetPosition = new ChessPosition('e', 5);
+
+        chessMatch.performChessMove(sourcePosition, targetPosition);
+
+        Assert.assertTrue(chessMatch.getBoard().thereIsAPiece(targetPosition.toPosition()));
+    }
+
+    @Test
+    public void givenAChessMovePieceWhenTargetPositionHasAPieceThenReturnThisPiece(){
+        ChessMatch chessMatch = new ChessMatch();
+        ChessPosition sourcePosition = new ChessPosition('e', 8);
+        ChessPosition targetPosition = new ChessPosition('e', 1);
+
+        ChessPiece capturedPiece = chessMatch.performChessMove(sourcePosition, targetPosition);
+
+        Assert.assertNotNull(capturedPiece);
     }
 }
