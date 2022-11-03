9ab37327405a297f89fa960c04c31f6fe509fe84
Bernardo Rodrigues <bernardo.rodrigues@capco.com>
2022-11-03 15:36:45 -0300

    feat: add piece move count

---
+++ src/test/java/chess/ChessPieceTest.java
@@ -0,0 +1,20 @@
+package chess;
+
+import org.junit.Assert;
+import org.junit.Test;
+import pieces.Rook;
+
+public class ChessPieceTest {
+    @Test
+    public void givenAPieceMovementWhenMovedCorrectlyThenUpdateItsMoveCount(){
+        ChessMatch chessMatch = new ChessMatch();
+        ChessPosition sourcePosition = new ChessPosition('e', 4);
+        ChessPosition targetPosition = new ChessPosition('e', 5);
+        Rook rook = new Rook(chessMatch.getBoard(), Color.WHITE);
+        chessMatch.getBoard().placePiece(rook, sourcePosition.toPosition());
+
+        chessMatch.performChessMove(sourcePosition, targetPosition);
+
+        Assert.assertEquals(rook.getMoveCount(), 1);
+    }
+}
