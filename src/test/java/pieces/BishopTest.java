aa54c45185d6f7e73263837cee0f970635d81355
Bernardo Rodrigues <bernardo.rodrigues@capco.com>
2022-11-03 15:49:44 -0300

    feat: add bishop

---
+++ src/test/java/pieces/BishopTest.java
@@ -0,0 +1,17 @@
+package pieces;
+
+import boardgame.Board;
+import chess.Color;
+import chess.pieces.King;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class BishopTest {
+    @Test
+    public void givenABishopWhenCallToStringThenReturnB(){
+        Board board = new Board(8,8);
+        King king = new King(board, Color.BLACK);
+
+        Assert.assertEquals(king.toString(), "B");
+    }
+}
